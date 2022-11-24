ab4cb7ef18783b0a3c0430fbe89094b416c75c5d
vindexTOS <110175298+vindexTOS@users.noreply.github.com>
2022-11-24 23:16:07 +0400

    weather API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useReducer } from "react";
+import axios from "axios";
+import cloudeSky from "./utils/cloudyafternoon.jpg";
+import clearSky from "./utils/clearsky.jpg";
+import rainSky from "./utils/rainsky.jpg";
+import {
+  BsSun,
+  BsFillCloudSunFill,
+  BsFillCloudSleetFill,
+} from "react-icons/bs";
 
 function App() {
+  const [location, setLocation] = useState("");
+  const [data, setData] = useState({});
+  const [weather, setWeather] = useState();
+
+  const url = `446b1001cea52469f7ca74f028b594b6`;
+
+  const getLocation = (event) => {
+    if (event.key === "Enter") {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=${url}`
+        )
+        .then((response) => {
+          setData(response);
+          console.log(data);
+        });
+      setWeather(data.data.weather[0].main);
+      console.log(weather);
+    }
+  };
+
+  const dataCheck = data.data === undefined;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="flex flex-col items-center  "
+      style={{
+        backgroundImage: `url(${
+          weather === "Clear"
+            ? clearSky
+            : weather === "Rain"
+            ? rainSky
+            : weather === "Clouds"
+            ? cloudeSky
+            : rainSky
+        })`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        height: "100vh",
+      }}
+    >
+      <div className=" flex items-center justify-center mt-5 w-[30%] h-[2rem] rounded-[15px] bg-sky-500/[.6] focus:outline-none">
+        <input
+          className="     w-[90%] h-[2rem] rounded-[15px] bg-sky-500/[.01] focus:outline-none"
+          placeholder="           Search your location"
+          onKeyPress={getLocation}
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          type="text"
+        />
+      </div>
+      <h1>{dataCheck ? "No Location Found" : data.data.name} </h1>
+
+      <div className="flex flex-col items-center justify-center h-[100vh] ">
+        <h1 className="text-[5rem]">
+          {dataCheck ? "00 " : data.data.main.temp}℃
+        </h1>
+        <div className="flex flex-row gap-3">
+          <h1 className="text-[1.2rem]">{dataCheck ? "No Data " : weather}</h1>
+          <p className="items-center flex justify-center">
+            {dataCheck ? null : weather === "Clear" ? (
+              <BsSun />
+            ) : weather === "Clouds" ? (
+              <BsFillCloudSunFill />
+            ) : weather === "Rain" ? (
+              <BsFillCloudSleetFill />
+            ) : null}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
